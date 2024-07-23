039234ad2880711d2b839e6f10ffa1487f32a1f4
Ray <ray9015@gmail.com>
2024-07-23 11:47:56 +0800

    :art: update response status

--- lib/mitake/response.rb
+++ lib/mitake/response.rb
@@ -33,7 +33,7 @@ module Mitake
     # @since 0.2.0
     # @api private
     def status
-      Status::CODES[@status_code]
+      Status::CODES[@status_code.to_i]
     end
 
     # Does message is duplicate
@@ -43,6 +43,24 @@ module Mitake
     # @since 0.2.0
     def duplicate?
       @duplicate == true
+    end
+
+    # Does message is success
+    #
+    # @return [TrueClass|FalseClass] Does message is success
+    #
+    # @since 0.2.0
+    def success?
+      ['1', '2', '4'].include?(@status_code)
+    end
+
+    # Does message is failed
+    #
+    # @return [TrueClass|FalseClass] Does message is failed
+    #
+    # @since 0.2.0
+    def failed?
+      !success?
     end
   end
 end
